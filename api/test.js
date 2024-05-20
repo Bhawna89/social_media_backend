88edf1b2bec213b27010af44154c25210446ccd5
bhawna89 <21bcs057@iiitdmj.ac.in>
2024-05-20 08:29:55 +0530

    cors

--- api/test.js
+++ api/test.js
@@ -1,12 +1,6 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
 
-app.use(cors({
-    origin: '*', // Allow all origins
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow these methods
-    allowedHeaders: ['Content-Type', 'Authorization'] // Allow these headers
-  }));
 app.get('/api/test', (req, res) => {
   res.send('Server is working!');
 });
