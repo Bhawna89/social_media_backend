47ac2cab3dead4974afd063798e8302cb77690c6
bhawna89 <21bcs057@iiitdmj.ac.in>
2024-05-21 08:32:19 +0530

    cors

--- api/test.js
+++ api/test.js
@@ -3,7 +3,7 @@ const app = express();
 
 const cors = require('cors');
 app.use(cors({
-  origin: 'http://your-allowed-origin.com',
+  origin: '*',
   methods: 'GET,POST,PUT,DELETE,OPTIONS',
   allowedHeaders: 'Content-Type,Authorization',
   credentials: true
